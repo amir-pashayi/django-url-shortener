abf744f126e6aa4a899188d652c44d6c5d5fa038
Amir Pashayi <amir.pasha.dev@gmail.com>
2025-09-21 21:03:21 +0330

    Add custom User model

--- urlshortner/accounts/admin.py
+++ urlshortner/accounts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ("phone", "username", "full_name", "email", "is_active", "is_staff", "is_superuser")
+    list_filter = ("is_active", "is_staff", "is_superuser", "gender")
+    search_fields = ("phone", "username", "full_name", "email")
+    ordering = ("-id",)
+    readonly_fields = ("last_login",)
+
+    fieldsets = (
+        (None, {"fields": ("phone", "password")}),
+        ("Personal info", {"fields": ("username", "full_name", "email", "bio", "age", "gender", "last_login")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("phone", "username", "age", "password1", "password2"),
+        }),
+    )
+
+    filter_horizontal = ("groups", "user_permissions")
