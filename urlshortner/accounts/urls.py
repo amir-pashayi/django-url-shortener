6ed1ffdb0aafe9f74b2083fc5c26816c3265ee0a
Amir Pashayi <amir.pasha.dev@gmail.com>
2025-09-23 19:39:04 +0330

    change navbar.html

--- urlshortner/accounts/urls.py
+++ urlshortner/accounts/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('verify/', views.LoginVerifyView.as_view(), name='verify'),
-    path('complete-register/', views.CompleteRegisterView.as_view(), name='complete-register')
+    path('complete-register/', views.CompleteRegisterView.as_view(), name='complete-register'),
     path("logout/", views.LogoutView.as_view(), name="logout"),
 ]
